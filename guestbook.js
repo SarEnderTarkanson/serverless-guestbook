3ac508396fdf60f57cb4e3643c204938bb7d5769
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2022-06-20 02:54:59 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://64f874e2.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
